264383ae5af5f777b5001b2c8e64f7ce32777259
AlanoBurity <alanosantosdev@gmail.com>
2022-07-18 15:52:41 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWhithRouter';
+
+describe('Teste o componente not Found', () => {
+  it('Teste se a página contém um heading `h2` com o texto `Page requested not found 😭`',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/pagina/que-nao-existe/');
+
+      const notFoundTitle = screen.getByRole('heading',
+        { name: /Page requested not found/i });
+      expect(notFoundTitle).toBeInTheDocument();
+    });
+
+  it('Teste se a página mostra a imagem do pikachu chorando', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/not-found');
+
+    const img = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
